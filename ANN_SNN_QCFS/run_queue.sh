c4bcb7d358ad9f80c284526b6e10884e613ef881
siristol <siri.stolevik@gmail.com>
2026-03-10 16:06:55 +0100

    run queue

--- ANN_SNN_QCFS/run_queue.sh
+++ ANN_SNN_QCFS/run_queue.sh
@@ -1,10 +1,6 @@
 
-python main_test.py -b=128 --dataset=cifar10 --model=resnet20 -id=resnet20_L[8] -T=1
-python main_test.py -b=128 --dataset=cifar10 --model=resnet20 -id=resnet20_L[8] -T=2
-python main_test.py -b=128 --dataset=cifar10 --model=resnet20 -id=resnet20_L[8] -T=4
-python main_test.py -b=128 --dataset=cifar10 --model=resnet20 -id=resnet20_L[8] -T=8
-python main_test.py -b=128 --dataset=cifar10 --model=resnet20 -id=resnet20_L[8] -T=16
-python main_test.py -b=128 --dataset=cifar10 --model=resnet20 -id=resnet20_L[8] -T=32
+python main_train.py -b=160 --dataset=cifar10 --model=mobilenet -lr=0.1 --epochs=200 -L=8 -wd=0.0005
 
+python main_train.py -b=160 --dataset=cifar100 --model=mobilenet -lr=0.05 --epochs=200 -L=8 -wd=0.0005
 
-
+python main_train.py -b=32 --dataset=coco --model=mobilenet -lr=0.001 --epochs=50 -L=8 -wd=0.0005
