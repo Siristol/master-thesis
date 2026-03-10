9238a7f73f0c772e82b50ba68f5f605ec2c7ed37
siristol <siri.stolevik@gmail.com>
2026-03-10 16:07:41 +0100

    add num filters and stride parameters

--- ANN_SNN_QCFS/Models/__init__.py
+++ ANN_SNN_QCFS/Models/__init__.py
@@ -8,8 +8,16 @@ def modelpool(MODELNAME, DATANAME):
         num_classes = 1000
     elif '100' in DATANAME.lower():
         num_classes = 100
-    else:
+        num_filters = 32
+        strideFistConv = 1
+    elif '10' in DATANAME.lower():
         num_classes = 10
+        num_filters = 32
+        strideFistConv = 1
+    elif 'coco' in DATANAME.lower():
+        num_classes = 2
+        num_filters = 8
+        strideFistConv = 2
     if MODELNAME.lower() == 'vgg16':
         return vgg16(num_classes=num_classes)
     elif MODELNAME.lower() == 'vgg19':
@@ -23,7 +31,7 @@ def modelpool(MODELNAME, DATANAME):
     elif MODELNAME.lower() == 'resnet20':
         return resnet20(num_classes=num_classes)
     elif MODELNAME.lower() == 'mobilenet':
-        return MobileNet(num_classes=num_classes)
+        return MobileNet(num_classes=num_classes, num_filters=num_filters, strideFistConv=strideFistConv)
     else:
         print("still not support this model")
         exit(0)
